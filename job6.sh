bcb223fb6bb4cf754af9374ab5a2a976ddb65bcf
Mahsa Bazzaz <mahsa.bazzaz@gmail.com>
2024-03-21 20:41:56 -0400

    job

---
+++ job6.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# SLURM directives
+#SBATCH --partition=short
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=50G
+#SBATCH --time=11:59:00
+#SBATCH --job-name=myjob
+#SBATCH --output=./log/output_%j.txt
+#SBATCH --error=./log/error_%j.txt
+
+module load anaconda3/2022.05
+module load python/3.8.1
+pip install -r requirements.txt
+
+command5="python pytorch/main.py"
+$command5 > logs/CGAN.txt
